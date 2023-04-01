5828f6cd6f44a0f50ac6e15b5c00b76d4d77f082
Abakpa Dominic <abakpad82@gmail.com>
2023-04-01 18:54:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,123 +1,115 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, Blueprint, render_template, redirect, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_
 from datetime import datetime
-from forms import RegistrationForm, LoginForm, VoteForm
-from authentication import login_required, current_user, logout_user
-from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
-from flask_migrate import Migrate
-from sqlalchemy.exc import IntegrityError
-from flask_cors import CORS, cross_origin
-import os
+from forms import LoginForm, VoteForm, RegistrationForm
+from models import User, Candidate, Vote
+from werkzeug.security import generate_password_hash
+import bcrypt
+import random
+import string
 
-def create_app():
-    from .models import db, User, Position, Candidate, Vote
-    db.init_app(app)
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JSON_SORT_KEYS'] = False
-    db =SQLAlchemy(app)
-    migrate = Migrate(app, db)
-    cors = CORS(app)
+auth = Blueprint('auth', __name__)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
 
+db = SQLAlchemy(app)
+
+# CORS setup
+from flask_cors import CORS
+CORS(app)
+
+# Define routes
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = SignUpForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        names = form.names.data
-        password = form.password.data
-        user = User(email=email, names=names, password=password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('signup.html', title='Sign Up', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        user = User.query.filter_by(email=email).first()
-        if user and user.check_password(password):
-            flash('You have been logged in!', 'success')
-            session['user_id'] = user.id
-            return redirect(url_for('dashboard'))
+        user = User.query.filter_by(voter_id=form.voter_id.data).first()
+        if user and bcrypt.checkpw(form.voter_key.data.encode(), user.voter_key.encode()):
+            session['user'] = user.id
+            session.permanent = True
+            return redirect('/dashboard')
         else:
-            flash('Login unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Log In', form=form)
+            flash('Invalid Voter ID or Key', 'error')
+    return render_template('login.html', form=form)
 
 @app.route('/logout')
 def logout():
-    logout_user()
-    return redirect(url_for('index'))
+    session.clear()
+    return redirect('/')
 
 @app.route('/dashboard')
-@login_required
 def dashboard():
-    positions = Position.query.all()
-    votes = Vote.query.all()
-    return render_template('dashboard.html', title='Dashboard', positions=positions, votes=votes)
-
-@app.route('/candidates')
-@cross_origin()
-def candidates():
-    candidates = Candidate.query.all()
-    return jsonify({'candidates': [{'id': c.id, 'name': c.name, 'position': c.position} for c in candidates]})
+    if 'user' not in session:
+        return redirect('/login')
+    user = User.query.get(session['user'])
+    if not user:
+        return redirect('/login')
+    if not user.has_voted:
+        positions = Candidate.query.with_entities(Candidate.position).distinct().all()
+        return render_template('vote.html', positions=positions)
+    else:
+        votes = Vote.query.filter_by(user_id=user.id).all()
+        return render_template('results.html', votes=votes)
 
 @app.route('/vote', methods=['GET', 'POST'])
-@login_required
 def vote():
+    if 'user' not in session:
+        return redirect('/login')
+    user = User.query.get(session['user'])
+    if not user:
+        return redirect('/login')
+    if user.has_voted:
+        flash('You have already voted', 'error')
+        return redirect('/dashboard')
     form = VoteForm()
     if form.validate_on_submit():
-        position_id = form.position.data
-        candidate_id = form.candidate.data
-        user_id = current_user.id
-        existing_vote = Vote.query.filter_by(user_id=user_id, position_id=position_id).first()
-        if existing_vote:
-            flash('You have already voted for this position', 'danger')
+        for position, candidate_id in form.data.items():
+            if position.startswith('position_'):
+                position_name = position.split('position_')[1]
+                candidate = Candidate.query.filter_by(id=candidate_id).first()
+                if candidate:
+                    vote = Vote(user_id=user.id, position=position_name, candidate_id=candidate_id)
+                    db.session.add(vote)
+        user.has_voted = True
+        db.session.commit()
+        flash('Vote casted successfully!', 'success')
+        return redirect('/dashboard')
+    else:
+        positions = Candidate.query.with_entities(Candidate.position).distinct().all()
+        candidates = {}
+        for position in positions:
+            candidates[position[0]] = Candidate.query.filter_by(position=position[0]).all()
+        return render_template('vote.html', positions=positions, candidates=candidates, form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            flash('Email address already exists')
+            return redirect(url_for('auth.register'))
         else:
-            vote = Vote(user_id=user_id, position_id=position_id, candidate_id=candidate_id)
-            db.session.add(vote)
-            db.session.commit()
-            flash('Your vote has been recorded!', 'success')
-            return redirect(url_for('dashboard'))
-    positions = Position.query.all()
-    form.position.choices = [(p.id, p.title) for p in positions]
-    form.candidate.choices = [(c.id, c.names) for c in Candidate.query.all()]
-    return render_template('vote.html', title='Vote', form=form)
-
-
-@app.route('/results')
-@login_required
-def results():
-    positions = Position.query.all()
-    votes = Vote.query.all()
-    results = []
-    for position in positions:
-        candidates = Candidate.query.filter_by(position_id=position.id).all()
-        position_results = {}
-        position_results['title'] = position.title
-        position_results['candidates'] = []
-        for candidate in candidates:
-            candidate_results = {}
-            candidate_results['name'] = candidate.names
-            candidate_results['votes'] = len([v for v in votes if v.candidate_id == candidate.id])
-            position_results['candidates'].append(candidate_results)
-        results.append(position_results)
-    return render_template('results.html', title='Results', results=results)
+            # generate a random voter id and key for the new user
+            voter_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+            voter_key = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+            
+            new_user = User(full_name=form.full_name.data, email=form.email.data,
+                            password=generate_password_hash(form.password.data, method='sha256'),
+                            voter_id=voter_id, voter_key=voter_key)
+            new_user.save()
+            flash('Registration successful. Please log in.')
+            return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
 
 @app.route('/')
 def application_great():
@@ -125,4 +117,3 @@ def application_great():
 
 if __name__ == 'main':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
