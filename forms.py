a4c0f8bc0d9d5640b611a335e2c2421c7caaec3f
Abakpa Dominic <abakpad82@gmail.com>
2023-04-01 13:51:43 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,27 +1,16 @@
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, RadioField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
 
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=15)])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    full_name = StringField('Full Name', validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
+    full_name = StringField('Full Name', validators=[DataRequired(), Length(max=255)])
+    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=255)])
+    submit = SubmitField('Register')
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    voter_id = StringField('Voter ID', validators=[DataRequired()])
+    voter_key = PasswordField('Voter Key', validators=[DataRequired()])
     submit = SubmitField('Login')
-
-class VoteForm(FlaskForm):
-    general_secretary = RadioField('General Secretary', choices=[('Joy Ifeanyi Dennis(John Wick)')], validators=[DataRequired()])
-    treasurer = RadioField('Treasurer', choices=[('Taiwo Elizabeth Fowowde')], validators=[DataRequired()])
-    sport_secretary = RadioField('Sport Secretary', choices=[('Alli_Kammal Mazeedah', 'Ayo Babalola(Ghost)', 'Farinloye Susan Folake')], validators=[DataRequired()])
-    public_relations_officer = RadioField('Public Relations Officer', choices=[('Annaun Samuel')], validators=[DataRequired()])
-    social_secretary = RadioField('Social Secretary', choices=[('Shoneye Omolola', 'Akintunde Itunuoluwa', 'Akinyomi Sefunmi Isaiah')], validators=[DataRequired()])
-    financial_secretary = RadioField('Financial Secretary', choices=[('Moghalu Chinyere')], validators=[DataRequired()])
-    welfare_secretary = RadioField('Welfare Secretary', choices=[('Yusuf Aminat', 'Victoria Olomola')], validators=[DataRequired()])
-    assistant_general_secretary = RadioField('Assistant General Secretary', choices=[('Oyeyinka Sarah')], validators=[DataRequired()])
-    submit = SubmitField('vote')
