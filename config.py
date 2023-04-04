47386e680d7ebf6332f1d58ca601ea795b5538d0
DT-GAMER <abakpad82@gmail.com>
2023-04-03 22:43:38 -0700

    update file

--- config.py
+++ config.py
@@ -17,16 +17,17 @@ class smpt:
     SMTP_SERVER = "smtp.gmail.com"
     SMTP_PORT = 587
 
-class ProductionConfig(Config):
+class ProductionConfig(config):
+    iiCONNECTION_STRING = 'postgres://electapp-main-db-0fa618ed4ebf577c7:tucb65V2BzwXGwjEBv28ME3HqcAak2@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/electapp-main-db-0fa618ed4ebf577c7'
     DEBUG = False
 
-class StagingConfig(Config):
+class StagingConfig(config):
     DEVELOPMENT = True
     DEBUG = True
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig(config):
     DEVELOPMENT = True
     DEBUG = True
 
-class TestingConfig(Config):
+class TestingConfig(config):
     TESTING = True
