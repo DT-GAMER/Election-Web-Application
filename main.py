a991db8a768c3a9dd51e96371362d7139c8943f7
Abakpa Dominic <abakpad82@gmail.com>
2023-04-03 23:56:34 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from api import login, register, vote, dashboard
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
