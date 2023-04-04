c249e3664d58395daf402e563d0a5be556ed7f74
Abakpa Dominic <abakpad82@gmail.com>
2023-04-04 15:11:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
-class config:
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    CONNECTION_STRING = 'postgres://electapp-main-db-0fa618ed4ebf577c7:tucb65V2BzwXGwjEBv28ME3HqcAak2@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/electapp-main-db-0fa618ed4ebf577c7'.format(
-        USERNAME=os.environ.get('electapp-main-db-0fa618ed4ebf577c7'),
-        PASSWORD=os.environ.get('tucb65V2BzwXGwjEBv28ME3HqcAak2'),
-        DATABASE_HOST=os.environ.get('user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com'),
-        DATABASE_PORT=os.environ.get('5432'),
-        DATABASE_NAME=os.environ.get('electapp-main-db-0fa618ed4ebf577c7')
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class Config:
+    def __init__(self):
+        self.DEBUG = False
+        self.TESTING = False
+        self.CSRF_ENABLED = True
+        self.CONNECTION_STRING = 'postgres://{username}:{password}@{host}:{port}/{database}'.format(
+            username=os.environ.get('DB_USERNAME'),
+            password=os.environ.get('DB_PASSWORD'),
+            host=os.environ.get('DB_HOST'),
+            port=os.environ.get('DB_PORT'),
+            database=os.environ.get('DB_NAME')
+        )
     
 class smpt:
     SMTP_SERVER = "smtp.gmail.com"
