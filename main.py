26cd9aaeaad2b2fb01ba17071624b5440d20171c
Abakpa Dominic <abakpad82@gmail.com>
2023-04-04 08:51:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from api import login, register, vote, dashboard
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+
 @app.route('/')
 def hello():
     return 'Hello, World!'
