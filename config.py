c7a992fcf6ff35bbda2393ac10feb45c7a5aad4a
Abakpa Dominic <abakpad82@gmail.com>
2023-04-07 22:38:08 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ class Config:
         self.DEBUG = False
         self.TESTING = False
         self.CSRF_ENABLED = True
-        self.CONNECTION_STRING = 'postgres://{username}:{password}@{host}:{port}/{database}'.format(
-            username=os.environ.get('DB_USERNAME'),
-            password=os.environ.get('DB_PASSWORD'),
-            host=os.environ.get('DB_HOST'),
-            port=os.environ.get('DB_PORT'),
-            database=os.environ.get('DB_NAME')
+        self.CONNECTION_STRING = 'postgres://electoralcollege-main-db-00e2a4d1aa8d4f008:AgaEqVczsdQYytD362kWxUvSd5JsWc@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/electoralcollege-main-db-00e2a4d1aa8d4f008'.format(
+            username=os.environ.get('electoralcollege-main-db-00e2a4d1aa8d4f008'),
+            password=os.environ.get('AgaEqVczsdQYytD362kWxUvSd5JsWc'),
+            host=os.environ.get('user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com'),
+            port=os.environ.get('5432'),
+            database=os.environ.get('electoralcollege-main-db-00e2a4d1aa8d4f008')
         )
         self.SMTP_SERVER = "smtp.gmail.com"
         self.SMTP_PORT = 587
