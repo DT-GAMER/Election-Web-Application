3e83254c3a4cfc97020a619e330dba8c0d2464ca
Abakpa Dominic <abakpad82@gmail.com>
2023-04-07 21:46:42 +0100

    Update register.py

--- register.py
+++ register.py
@@ -5,57 +5,17 @@ import string
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import config
-
-class Config:
-    class SMTP:
-        def __init__(self):
-            self.SMTP_SERVER = "smtp.gmail.com"
-            self.SMTP_PORT = 587
-    
-    def __init__(self):
-        self.DEBUG = False
-        self.TESTING = False
-        self.CSRF_ENABLED = True
-        self.CONNECTION_STRING = 'postgres://{username}:{password}@{host}:{port}/{database}'.format(
-            username=os.environ.get('DB_USERNAME'),
-            password=os.environ.get('DB_PASSWORD'),
-            host=os.environ.get('DB_HOST'),
-            port=os.environ.get('DB_PORT'),
-            database=os.environ.get('DB_NAME')
-        )
-        self.SMTP = Config.SMTP()
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
-
+from config import Config
 
 # SMTP email server parameters
-SMTP_SERVER = config.Config.SMTP.SMTP_SERVER
-SMTP_PORT = config.Config.SMTP.SMTP_PORT
+SMTP_SERVER = Config().SMTP_SERVER
+SMTP_PORT = Config().SMTP_PORT
 EMAIL_ADDRESS = "theelectoralcollege24@gmail.com"
 EMAIL_PASSWORD = "electoralcollege2023"
-
 
 def generate_key(length=24):
     """Generate a random key of a given length."""
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
-
 
 def send_registration_email(email, key):
     """Send a registration email with the given key to the given email address."""
@@ -72,14 +32,13 @@ def send_registration_email(email, key):
         server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
         server.sendmail(EMAIL_ADDRESS, email, message.as_string())
 
-
 def register_user(full_name, email):
     """Register a new user with the given full name and email address."""
     key = generate_key()
 
     try:
         # Connect to the database
-        conn = psycopg2.connect(config.Config.CONNECTION_STRING)
+        conn = psycopg2.connect(Config().CONNECTION_STRING)
         cur = conn.cursor()
 
         # Insert the new user into the database
