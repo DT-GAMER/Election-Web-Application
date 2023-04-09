81e615878a1ea48f23920f39746793757ee0e98f
Abakpa Dominic <abakpad82@gmail.com>
2023-04-09 15:10:01 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,26 @@
 import os
 
 class Config:
-    def __init__(self, username='default_username', password='default_password', host=None, database='default_database'):
+    def __init__(self, username, password, database, host=None):
         self.DEBUG = False
         self.TESTING = False
         self.CSRF_ENABLED = True
         self.CONNECTION_STRING = 'postgres://{username}:{password}@{host}/{database}'.format(
             username=username,
             password=password,
-            host=host or '',
+            host=host or 'localhost',
             database=database
         )
         self.SMTP_SERVER = "smtp.gmail.com"
         self.SMTP_PORT = 587
 
-config = Config()
-config.username = 'electoralcollege-main-db-00e2a4d1aa8d4f008'
-config.password = 'AgaEqVczsdQYytD362kWxUvSd5JsWc'
-config.host = 'user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com'
-config.database = 'electoralcollege-main-db-00e2a4d1aa8d4f008'
+
+config = Config(
+    username='electoralcollege-main-db-00e2a4d1aa8d4f008',
+    password='AgaEqVczsdQYytD362kWxUvSd5JsWc',
+    database='electoralcollege-main-db-00e2a4d1aa8d4f008',
+    host='user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com'
+)
 
 class ProductionConfig(Config):
     DEBUG = False
